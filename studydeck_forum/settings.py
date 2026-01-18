1655d9b7cc258bcae1845fecdec7c9392fe5fd39
diptansuroy <f20250804@pilani.bits-pilani.ac.in>
2026-01-19 01:25:06 +0530

    Update settings.py

--- studydeck_forum/settings.py
+++ studydeck_forum/settings.py
@@ -71,8 +71,8 @@ WSGI_APPLICATION = 'studydeck_forum.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
-        conn_max_age=600
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
